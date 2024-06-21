683b71a7945ec647fdebc1756feb297b8c9c5490
PymPhekasut <74609915+PymPhekasut@users.noreply.github.com>
2024-06-21 11:38:12 +1000

    Delete Project/Config.py

--- Project/Config.py
+++
@@ -1,3 +0,0 @@
-Channel_secret = '2b576dfb70e190897c6ea796dc0972c0'
-Channel_access_token = '1RWGug0rbJSCbd9u/kkTOVymfYC7RSIhZSKzseG6Bgz+6IN+5pOOoUddJERy/2FDAe49dy8KXuWfbPE+zvjWK3zgnkV3IhuKixwvAAGKjHSppKUyYEHH9I/WSWyNWJDi+ZEQxFGt+o+4mxhlRqg5hwdB04t89/1O/w1cDnyilFU='
-basic_id = '@518mbzfg'
